2b57c3ebec832903aea123448c4066edd9b4134c
johnboeira <jbiel72001@gmail.com>
2021-04-11 23:59:09 -0300

    Robo V1

---
+++ Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoboAEB
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int contadorGrid = 0;
+           
+
+                Robo robo1 = new Robo();
+                Robo robo2 = new Robo();
+
+                if (contadorGrid < 1)
+                {
+                    Console.WriteLine("Entre com o tamanho do grid nesse formato (x y)");
+                    string grid = Console.ReadLine();
+                    contadorGrid++;
+                    robo1.gerarGrid(grid);
+                    robo2.gerarGrid(grid);
+                   
+                }
+                for (int i = 0; i < 1; i++)
+                {
+                    robo1.pedirPosicao();
+
+                    do
+                    {
+                        string comando = "";
+                        Console.WriteLine("Entre comando:");
+                        comando = Console.ReadLine();
+
+                        robo1.interpretador(comando);
+                        if (robo1.validacaParaGrid == true && robo1.validacaoDEM == true)
+                        {
+                            Console.Write(robo1.x + " ");
+                            Console.Write(robo1.y + " ");
+                            Console.Write(robo1.orientacao);
+                            Console.ReadLine();
+                            Console.Clear();
+                            break;
+                        }
+                        else if (robo1.validacaParaGrid == false || robo1.validacaoDEM == false)
+                        {
+                            
+                            Console.WriteLine("Comando inválido");
+                            Console.ReadLine();
+                            
+                            Console.Clear();
+                            
+
+                        }
+
+                    } while (robo1.validacaParaGrid == false || robo1.validacaoDEM == false);
+
+                }
+                for (int i = 0; i < 1; i++)
+                {
+                    Console.WriteLine("Segundo robo:");
+                    robo2.pedirPosicao();
+
+                    do
+                    {
+                        string comando = "";
+                        Console.WriteLine("Entre comando:");
+                        comando = Console.ReadLine();
+
+                        robo2.interpretador(comando);
+                        if (robo2.validacaParaGrid == true && robo2.validacaoDEM == true)
+                        {
+                            Console.Write(robo2.x + " ");
+                            Console.Write(robo2.y + " ");
+                            Console.Write(robo2.orientacao);
+                            Console.ReadLine();
+                            Console.Clear();
+                            break;
+                        }
+                        else if (robo2.validacaParaGrid == false || robo2.validacaoDEM == false)
+                        {
+                            Console.WriteLine("Comando inválido: valor não corresponde a: D,E ou M!");
+                            Console.ReadLine();
+                            Console.Clear();
+                        }
+
+                    } while (robo2.validacaParaGrid == false || robo2.validacaoDEM == false);
+
+                }
+                
+
+
+
+
+
+
+
+
+
+            }
+        }
+    }
+
---
+++ Robo.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoboAEB
+{
+    public class Robo
+    {
+        public uint x = 0, y = 0;
+        public char orientacao;
+        public uint xgrid = 0, ygrid = 0;
+        public bool validacaParaGrid = true, validacaoDEM = true;
+        public string posicaoInicial;       
+
+        public void gerarGrid(string grid)
+        {
+            grid.Replace(" ", "");
+            char[] charGrid = grid.ToCharArray();
+
+            xgrid = (uint)Char.GetNumericValue(charGrid[0]);
+            ygrid = (uint)Char.GetNumericValue(charGrid[2]);
+            
+        }
+
+        public void pedirPosicao()
+        {
+
+            Console.WriteLine("Entre com a posição inicial: ");
+            posicaoInicial = Console.ReadLine();
+            char[] charPos = posicaoInicial.ToCharArray();
+            char orientac = posicaoInicial[4];
+
+            
+
+             uint px= (uint)Char.GetNumericValue(charPos[0]);
+            uint py = (uint)Char.GetNumericValue(charPos[2]);
+
+            gerarPosicao(px, py, orientac);
+        }
+
+        public void gerarPosicao(uint px, uint py, char orientac)
+        {
+            x = px;
+            y = py;
+            
+            orientacao = orientac;
+        }
+
+        public void interpretador(string comando)
+        {
+            string cmd = comando.ToUpper();
+            char[] charCmd = cmd.ToCharArray();
+
+            foreach (var ch in charCmd)
+            {
+                if (ch == 'E')
+                {
+                    validacaoDEM = true;
+                    if (orientacao == 'N' || orientacao == 'n')
+                    {
+                        orientacao = 'O';
+                    }
+                    else if (orientacao == 'L' || orientacao == 'l')
+                    {
+                        orientacao = 'N';
+                    }
+                    else if (orientacao == 'S' || orientacao == 's')
+                    {
+                        orientacao = 'L';
+                    }
+                    else if (orientacao == 'O' || orientacao == 'o')
+                    {
+                        orientacao = 'S';
+                    }
+                }
+                if (ch == 'D')
+                {
+                    validacaoDEM = true;
+                    if (orientacao == 'N' || orientacao == 'n')
+                    {
+                        orientacao = 'L';
+                    }
+                    else if (orientacao == 'L' || orientacao == 'l')
+                    {
+                        orientacao = 'S';
+                    }
+                    else if (orientacao == 'S' || orientacao == 's')
+                    {
+                        orientacao = 'O';
+                    }
+                    else if (orientacao == 'O' || orientacao == 'o')
+                    {
+                        orientacao = 'N';
+                    }
+                }
+                if (ch == 'M')
+                {
+                    validacaoDEM = true;
+                    if (orientacao == 'N' || orientacao == 'n')
+                    {
+                        if (y <= ygrid)
+                        {
+                            y++;                          
+                        }
+                        else
+                        {
+                            
+                            validacaParaGrid = false;
+                            break;
+                            
+                        }
+                    }
+
+                    else if (orientacao == 'L' || orientacao == 'l')
+                    {
+                        if (x <= xgrid)
+                        {
+                            x++;
+                        }
+                        else
+                        {
+                            
+                            validacaParaGrid = false;
+                            break;
+                            
+                        }
+                    }
+
+                    else if (orientacao == 'S' || orientacao == 's')
+                    {
+                        if (y != 0)
+                        {
+                            y--;
+                        }
+                        else
+                        {
+                            
+                            validacaParaGrid = false;
+                            break;
+                           
+                        }
+                    }
+                    else if (orientacao == 'O' || orientacao == 'o')
+                    {
+                        if (x != 0)
+                        {
+                            x--;
+                        }
+                        else
+                        {
+                            
+                            validacaParaGrid = false;
+                            break;
+                            
+                        }
+                    }
+                }
+                else if (ch != 'M')
+                {
+                    if (ch != 'D')
+                    {
+                        if (ch != 'E')
+                        {
+                            
+                            validacaoDEM = false;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
